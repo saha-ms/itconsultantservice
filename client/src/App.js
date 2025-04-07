cf5d8471ceda455d94ab1e55d5e41202e0a95891
Saha <md.sahabuddin@hotmail.com>
2025-04-07 22:16:23 +0200

    Updated content in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Saha: Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>Welcome to My IT Services Platform!</h1>
+          <p>Offering cutting-edge solutions to meet all your IT requirements.</p>
+      </div>
   );
 }
-
 export default App;
