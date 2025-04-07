bcdeb16293c4549d0ab6d301a451ee26076dfb48
Saha <md.sahabuddin@hotmail.com>
2025-04-07 22:45:39 +0200

    Update App.test.js to match App.js content

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,10 +4,9 @@ import App from './App';
 
 test('renders welcome message', () => {
   render(<App />);
-  const headerElement = screen.getByText(/welcome to my it services platform!/i);
+  const headerElement = screen.getByRole('heading', { name: /welcome to it services platform!/i });
   expect(headerElement).toBeInTheDocument();
-
-  const paragraphElement = screen.getByText(/offering cutting-edge solutions to meet all your it requirements./i);
-  expect(paragraphElement).toBeInTheDocument();
 });
+
+
 
