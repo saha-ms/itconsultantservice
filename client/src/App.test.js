c4cb6ccf44002a9dedaf6eccf9d35bec767759c9
Saha <md.sahabuddin@hotmail.com>
2025-04-07 22:35:34 +0200

    Automated Deployment after update App.test.jsto GitHub Pages

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/welcome to my it services platform!/i);
+  expect(headerElement).toBeInTheDocument();
+
+  const paragraphElement = screen.getByText(/offering cutting-edge solutions to meet all your it requirements./i);
+  expect(paragraphElement).toBeInTheDocument();
 });
+
